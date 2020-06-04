17c368fb065f10a7491582943813e975e3278614
ryuta kono <otami0103@gmail.com>
2020-06-04 16:12:39 +0900

    add cmake-mode

---
+++ elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "cc746c5b7e565fd7240a87277e2dacf8d7f49ab1")
