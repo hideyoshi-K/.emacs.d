32971827a9f73f9a03829c8c18a0a5f8019c34e9
kono <otami0103@gmail.com>
2023-02-03 14:12:45 +0900

    パッケージ更新

---
+++ elpa/compat-29.1.3.2/compat-autoloads.el
@@ -0,0 +1,17 @@
+;;; compat-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("compat-25.el" "compat-26.el" "compat-27.el"
+;;;;;;  "compat-28.el" "compat-29.el" "compat-macs.el" "compat-pkg.el"
+;;;;;;  "compat.el") (25564 38429 703252 591000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; compat-autoloads.el ends here
---
+++ elpa/zenburn-theme-20220927.631/zenburn-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zenburn-theme" "20220927.631" "A low contrast color theme for Emacs." 'nil :commit "4788de0bcfecf8faec69251decb9924492d008f3" :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "http://github.com/bbatsov/zenburn-emacs")
